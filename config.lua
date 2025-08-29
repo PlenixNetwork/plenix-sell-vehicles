e4278feb4b9e0c7b674d97c71382bd0ff3582944
alexasecas <alexbarraganq@hotmail.com>
2025-08-29 11:09:17 +0200

    [INIT]

---
+++ config.lua
@@ -0,0 +1,24 @@
+Config = {}
+
+Config.SellPoint = {
+    blipSprite = 147,
+    blipColor = 52,
+    blipScale = 0.6,
+    blipShortRange = true,
+    blipPos = vector3(-328.90002441406, -1321.0535888672, 32.282821655273),
+
+    markerType = 36,
+    markerColor = { r = 10, g = 255, b = 0, a = 80 },
+    markerWidth = 1.0,
+    markerHeight = 1.0,
+    markerPos = vector3(-328.90002441406, -1321.0535888672, 32.282821655273),
+    markerShowRadius = 10.0,
+}
+
+Config.ViewVehicles = {
+    position = vector4(-333.78848266602, -1282.4191894531, 31.442312240601, 19.642417907715),
+    spawnDistance = 50.0,
+}
+
+Config.ReturnCarToOwnerTax = 0.1 -- Default 10% tax for taking the car from the dealership
+Config.SellCarTax = 0.1 -- Default 10% tax for selling the car to the dealership
---
+++ server/main.lua
@@ -0,0 +1,222 @@
+lib.locale()
+
+lib.callback.register('fami-sell-vehicles:checkCar', function (source, vehicle, vehiclePlate)
+    local xPlayer = ESX.GetPlayerFromId(source)
+
+    local response = MySQL.query.await('SELECT owner FROM owned_vehicles WHERE owner = @owner AND plate = @plate LIMIT 1', {
+        ['@owner'] = xPlayer.getIdentifier(),
+        ['@plate'] = vehiclePlate
+    })
+
+    if response then
+        if #response > 0 then
+            return true
+        else
+            return false
+        end
+    else
+        return false
+    end
+end)
+
+lib.callback.register('fami-sell-vehicles:putOnSale', function(source, money, vehicleProps)
+    local xPlayer = ESX.GetPlayerFromId(source)
+    local plate = vehicleProps.plate
+
+    local response = MySQL.query.await('SELECT owner FROM owned_vehicles WHERE owner = @owner AND plate = @plate LIMIT 1', {
+        ['@owner'] = xPlayer.getIdentifier(),
+        ['@plate'] = plate
+    })
+
+    if not response then
+        return false
+    end
+
+    if #response == 0 then
+        return false
+    end
+
+    MySQL.insert.await('INSERT INTO vehicles_for_sale (seller, vehicleProps, price) VALUES (@seller, @vehicleProps, @price)', {
+        ['@seller'] = xPlayer.getIdentifier(),
+        ['@price'] = money,
+        ['@vehicleProps'] = json.encode(vehicleProps)
+    })
+
+    MySQL.update.await('DELETE FROM owned_vehicles WHERE plate = @plate', {
+        ['@plate'] = plate
+    })
+
+    return true
+end)
+
+lib.callback.register('fami-sell-vehicles:getNumberOfCars', function (source)
+    local xPlayer = ESX.GetPlayerFromId(source)
+    local plate = GetVehicleNumberPlateText(GetVehiclePedIsIn(GetPlayerPed(source), false))
+
+    local result = MySQL.query.await('SELECT COUNT(*) FROM vehicles_for_sale')
+
+    return result
+end)
+
+lib.callback.register('fami-sell-vehicles:getVehicleForSale', function (source, index)
+    if index ~= '0' then
+        index = tonumber(index)
+    else
+        index = 0
+    end
+
+    if index == nil then
+        error("Invalid index")
+    end
+
+    local result = MySQL.query.await('SELECT vehicleProps, seller, price FROM vehicles_for_sale LIMIT 1 OFFSET @index', {
+        ['@index'] = index
+    })
+
+    return result
+end)
+
+lib.callback.register('fami-sell-vehicles:getVehiclesForSale', function (source)
+    local result = MySQL.query.await('SELECT * FROM vehicles_for_sale')
+
+    local id = 0
+    for k, v in pairs(result) do
+        v.id = id
+        id = id + 1
+    end
+
+    return result
+end)
+
+RegisterNetEvent('fami-sell-vehicles:buyVehicle')
+AddEventHandler('fami-sell-vehicles:buyVehicle', function (index)
+    local xPlayer = ESX.GetPlayerFromId(source)
+
+    if index ~= '0' then
+        index = tonumber(index)
+    else
+        index = 0
+    end
+
+    if index == nil then
+        error("Invalid index")
+    end
+
+    local result = MySQL.query.await('SELECT id, vehicleProps, seller, price FROM vehicles_for_sale LIMIT 1 OFFSET @index', {
+        ['@index'] = index
+    })
+
+    if result[1] then
+        local vehicleProps = json.decode(result[1].vehicleProps)
+        local seller = result[1].seller
+        local price = result[1].price
+
+        if xPlayer.getMoney() >= price then
+            xPlayer.removeMoney(price)
+            UpdateCash(seller, price - (price * (Config.SellCarTax or 0.1)))
+
+            MySQL.insert.await([[
+                INSERT INTO owned_vehicles 
+                (owner, plate, vehicle, type, stored, in_garage, stored_in_garage, mileage) 
+                VALUES 
+                (@owner, @plate, @vehicle, "car", 1, 1, @location, @mileage)
+            ]], {
+                ['@owner'] = xPlayer.getIdentifier(),
+                ['@plate'] = vehicleProps.plate,
+                ['@vehicle'] = json.encode(vehicleProps),
+                ['@location'] = "San Andreas Avenue Elgin Avenue - Pillbox Hill",
+                ['@mileage'] = 0
+            })
+
+            MySQL.update.await('DELETE FROM vehicles_for_sale WHERE id = @id', {
+                ['@id'] = result[1].id
+            })
+
+            TriggerClientEvent('fami-sell-vehicles:removedVehicle', -1, index)
+
+            xPlayer.showNotification(locale('vehicle_bought', price))
+        else
+            xPlayer.showNotification(locale('not_enough_money'))
+        end
+    end
+end)
+
+RegisterNetEvent('fami-sell-vehicles:returnVehicle')
+AddEventHandler('fami-sell-vehicles:returnVehicle', function (index)
+    local xPlayer = ESX.GetPlayerFromId(source)
+
+    if index ~= '0' then
+        index = tonumber(index)
+    else
+        index = 0
+    end
+
+    if index == nil then
+        error("Invalid index")
+    end
+
+    local result = MySQL.query.await('SELECT id, vehicleProps, seller, price FROM vehicles_for_sale LIMIT 1 OFFSET @index', {
+        ['@index'] = index
+    })
+
+    if result[1] then
+        local vehicleProps = json.decode(result[1].vehicleProps)
+        local seller = result[1].seller
+        if seller ~= xPlayer.getIdentifier() then
+            xPlayer.showNotification(locale('not_your_vehicle'))
+            return
+        end
+
+        MySQL.insert.await([[
+            INSERT INTO owned_vehicles 
+            (owner, plate, vehicle, type, stored, in_garage, stored_in_garage, mileage) 
+            VALUES 
+            (@owner, @plate, @vehicle, 1, 1, @location, "car")
+        ]], {
+            ['@owner'] = seller,
+            ['@plate'] = vehicleProps.plate,
+            ['@vehicle'] = json.encode(vehicleProps),
+            ['@location'] = "San Andreas Avenue Elgin Avenue - Pillbox Hill",
+            ['@mileage'] = 0
+        })
+
+        MySQL.update.await('DELETE FROM vehicles_for_sale WHERE id = @id', {
+            ['@id'] = result[1].id
+        })
+
+        TriggerClientEvent('fami-sell-vehicles:removedVehicle', -1, index)
+
+        xPlayer.showNotification(locale('vehicle_returned'))
+    end
+end)
+
+function UpdateCash(identifier, cash)
+	local xPlayer = ESX.GetPlayerFromIdentifier(identifier)
+
+	if xPlayer ~= nil then
+		xPlayer.addAccountMoney("bank", cash)
+
+        xPlayer.showNotification(locale('vehicle_sold', cash))
+	else
+		MySQL.Async.fetchAll('SELECT accounts FROM users WHERE identifier = @identifier', { ["@identifier"] = identifier }, function(result)
+		    if result[1]["accounts"] ~= nil then
+	       		local accountsData = json.decode(result[1]["accounts"]) 
+        			accountsData["bank"] = accountsData["bank"] + cash
+
+        			MySQL.Async.execute("UPDATE users SET accounts = @newBank WHERE identifier = @identifier",
+            		{
+                			["@identifier"] = identifier,
+                			["@newBank"] = json.encode(accountsData) 
+            		})
+    		    end
+		end)
+	end
+end
+
+Trim = function(word)
+	if word ~= nil then
+		return word:match("^%s*(.-)%s*$")
+	else
+		return nil
+	end
+end
