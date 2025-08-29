4b44ec6621300acbe5cdd3553197905f4cb684a5
alexasecas <alexbarraganq@hotmail.com>
2025-08-29 11:39:59 +0200

    Update config.lua

--- config.lua
+++ config.lua
@@ -5,19 +5,19 @@ Config.SellPoint = {
     blipColor = 52,
     blipScale = 0.6,
     blipShortRange = true,
-    blipPos = vector3(-328.90002441406, -1321.0535888672, 32.282821655273),
+    blipPos = vector3(-23.2987, -1678.4564, 29.4658),
 
     markerType = 36,
     markerColor = { r = 10, g = 255, b = 0, a = 80 },
     markerWidth = 1.0,
     markerHeight = 1.0,
-    markerPos = vector3(-328.90002441406, -1321.0535888672, 32.282821655273),
+    markerPos = vector3(-23.2987, -1678.4564, 29.4658),
     markerShowRadius = 10.0,
 }
 
 Config.ViewVehicles = {
-    position = vector4(-333.78848266602, -1282.4191894531, 31.442312240601, 19.642417907715),
-    spawnDistance = 50.0,
+    position = vector4(-51.8511, -1685.3160, 29.4917, 316.3311),
+    spawnDistance = 30.0,
 }
 
 Config.ReturnCarToOwnerTax = 0.1 -- Default 10% tax for taking the car from the dealership
